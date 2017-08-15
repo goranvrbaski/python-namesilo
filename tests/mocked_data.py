66e71c05e3e3cd2491c44a53e26596ca11b847ea
Goran Vrbaški <vrbaski.goran@gmail.com>
2017-08-15 22:56:07 +0200

    improved tests

---
+++ tests/mocked_data.py
@@ -0,0 +1,41 @@
+mocked_data = {
+    'namesilo': {
+        'reply': {
+            'code': 300,
+            'detail': '',
+            'balance': '500',
+            'new_balance': "505",
+            'available': [],
+            'contact': ['some-contact', 'some-other-contact'],
+            'auto_renew': True,
+            'created': '',
+            'expires': '',
+            'locked': '',
+            'private': '',
+            'status': '',
+            'traffic_type': '',
+            'nameservers': {
+                'nameserver': [
+                    {
+                        '#text': 'name-server-1'
+                    },
+                    {
+                        '#text': 'name-server-2'
+                    },
+                    {
+                        '#text': 'name-server-3'
+                    }
+                ]
+            },
+            'contact_ids': {
+                'administrative': '',
+                'billing': '',
+                'registrant': '',
+                'technical': ''
+            },
+            'domains': {
+                'domain': ['some-example-domain.com', 'example.com']
+            }
+        }
+    }
+}
--- tests/namesilo.py
+++ tests/namesilo.py
@@ -1,63 +1,119 @@
-import os
 import unittest
 
-from uuid import uuid4
+from unittest import mock
 
-from main import NameSilo, ContactModel
+from main import NameSilo
 from common.models import DomainInfo
 from common.error_codes import check_error_code
+from tests.mocked_data import mocked_data
 
 
 class NamesiloTestCase(unittest.TestCase):
-    namesilo = NameSilo(os.environ['TOKEN'], sandbox=True)
+    def setUp(self):
+        self.namesilo = NameSilo("name-silo-token", sandbox=True)
 
-    def test_account_balance(self):
+    @mock.patch('main.NameSilo._get_content_xml')
+    @mock.patch('main.check_error_code')
+    @mock.patch('main.NameSilo._get_error_code')
+    def test_process_data(self, mock_xml, mock_check, mock_error_code):
+        self.namesilo._process_data("some-url-extend")
+        mock_xml.assert_called_once()
+        mock_check.assert_called_once()
+        mock_error_code.assert_called_once()
+
+    @mock.patch('main.requests.get')
+    def test_get_content_xml(self, mock_requests):
+        mock_response = mock.Mock()
+        mock_response.status_code = 200
+        mock_response.content = "<?xml version='1.0' encoding='UTF-8'?>" \
+                                "<example></example>".encode('utf-8')
+
+        mock_requests.return_value = mock_response
+        result = self.namesilo._get_content_xml('some_url_extend')
+        self.assertIsInstance(result, dict)
+
+    @mock.patch('main.requests.get')
+    def test_get_content_xml_exception(self, mock_requests):
+        mock_response = mock.Mock()
+        mock_response.status_code = 404
+        mock_requests.return_value = mock_response
+        self.assertRaises(Exception, self.namesilo._get_content_xml, 'url')
+
+    @mock.patch('main.NameSilo._get_content_xml')
+    def test_account_balance(self, mock_content_xml):
+        mock_content_xml.return_value = mocked_data
         balance = self.namesilo.get_account_balance()
         self.assertIsInstance(balance, float)
-        self.assertGreaterEqual(balance, 0)
+        self.assertEqual(balance, 500)
 
-    def test_add_funds(self):
-        balance = self.namesilo.get_account_balance()
-        expected_balance = round(balance + 5, 2)
+    @mock.patch('main.NameSilo._get_content_xml')
+    def test_add_funds(self, mock_content_xml):
+        mock_content_xml.return_value = mocked_data
         status, balance = self.namesilo.add_account_funds(5, 281)
-        self.assertEqual(expected_balance, balance)
+        self.assertEqual(balance, 505)
 
-    def test_domain_check_available(self):
-        domain_name = "{}.com".format(uuid4())
+    @mock.patch('main.NameSilo._get_content_xml')
+    def test_domain_check_available(self, mock_content_xml):
+        domain_name = "some-domain.com"
+        mock_content_xml.return_value = mocked_data
         self.assertTrue(self.namesilo.check_domain(domain_name))
 
-    def test_domain_check_not_available(self):
-        registered_domains = self.namesilo.list_domains()
-        self.assertFalse(self.namesilo.check_domain(registered_domains[0]))
+    @mock.patch('main.NameSilo._get_content_xml')
+    def test_domain_check_not_available(self, mock_content_xml):
+        domain_name = "some-domain.com"
+        del mocked_data['namesilo']['reply']['available']
+        mock_content_xml.return_value = mocked_data
+        self.assertFalse(self.namesilo.check_domain(domain_name))
 
-    def test_domain_registration(self):
-        domain_name = f"test-{uuid4()}.com"
+    @mock.patch('main.NameSilo._get_content_xml')
+    def test_domain_registration(self, mock_content_xml):
+        domain_name = "some-domain.com"
+        mock_content_xml.return_value = mocked_data
         self.assertTrue(self.namesilo.register_domain(domain_name))
 
-    def test_domain_renewal(self):
-        domain_name = self.namesilo.list_domains()[-30]
+    @mock.patch('main.NameSilo._get_content_xml')
+    def test_domain_renewal(self, mock_content_xml):
+        domain_name = "some-domain.com"
+        mocked_data['namesilo']['reply']['code'] = 300
+        mock_content_xml.return_value = mocked_data
         self.assertTrue(self.namesilo.renew_domain(domain_name))
 
-    def test_domain_registration_fail(self):
-        self.assertRaises(Exception, self.namesilo.register_domain)
+    @mock.patch('main.NameSilo._get_content_xml')
+    def test_list_domains(self, mock_content_xml):
+        mocked_data['namesilo']['reply']['code'] = 300
+        mock_content_xml.return_value = mocked_data
+        self.assertEqual(
+            self.namesilo.list_domains(),
+            mocked_data['namesilo']['reply']['domains']['domain']
+        )
 
-    def test_contacts_lists(self):
+    @mock.patch('main.NameSilo._get_content_xml')
+    def test_contacts_lists(self, mock_content_xml):
+        mock_content_xml.return_value = mocked_data
         self.assertIsInstance(self.namesilo.list_contacts(), list)
 
-    def test_domain_price(self):
+    @mock.patch('main.NameSilo._get_content_xml')
+    def test_domain_price(self, mock_content_xml):
+        mock_content_xml.return_value = mocked_data
         self.assertIsInstance(self.namesilo.get_prices(), dict)
 
     def test_check_error_code(self):
         self.assertIsInstance(check_error_code((300, "")), str)
 
-    def test_get_domain_info(self):
-        self.assertIsInstance(self.namesilo.get_domain_info(
-            self.namesilo.list_domains()[0]), DomainInfo
-        )
+    @mock.patch('main.NameSilo._get_content_xml')
+    def test_get_domain_info(self, mock_content_xml):
+        mock_content_xml.return_value = mocked_data
+        mocked_data['namesilo']['reply']['code'] = 300
+        self.assertIsInstance(self.namesilo.get_domain_info("some-domain.com"),
+                              DomainInfo)
 
-    def test_add_contact(self):
-        contact = ContactModel("First", "Last", "Address 15", "Some City", "Vojvodina", "RS", "test@nomail.com", "0038169999999", "21000")
-        self.assertTrue(self.namesilo.add_contact(contact))
+    @mock.patch('main.NameSilo._get_content_xml')
+    def test_domain_registration_fail(self, mock_content_xml):
+        domain_name = "some-domain.com"
+        mocked_data['namesilo']['reply']['code'] = 261
+        mock_content_xml.return_value = mocked_data
+        self.assertRaises(Exception, self.namesilo.register_domain,
+                          domain_name)
 
 if __name__ == '__main__':
     try:
